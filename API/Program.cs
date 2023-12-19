391b7784c447064dd3b32ca92c4cbaf413ea6967
Minh Nguyen <mnhngn20@gmail.com>
2023-12-19 17:46:09 +0700

    test

--- API/Program.cs
+++ API/Program.cs
@@ -1,32 +1,13 @@
-using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Interfaces;
 using API.Middlewares;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
-
-// Swagger
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-    };
-});
 
 var app = builder.Build();
 
@@ -48,6 +29,22 @@ app.UseAuthorization();
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
 
